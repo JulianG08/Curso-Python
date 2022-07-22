cd97c3f784beff45071a7f208c7808b2fff67999
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:36:08 -0500

    Agregando el archivo Ejercicio 002

---
+++ 01.Elementos basicos/Ejercicio 002.py
@@ -0,0 +1,7 @@
+'''
+    Se desea calcular el resultado de una operación
+'''
+a = float(input("Digita un número: "))
+b = float(input("Digita un número: "))
+resultado = ((3+5*8)<3) and ((-6/3 * 4)+2<2) or (a>b)
+print(f"El resultado es {resultado}")
