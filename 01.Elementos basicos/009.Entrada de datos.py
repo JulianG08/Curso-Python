d240e477f850c996a9421371b3e00aea79ff0ac5
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:33:34 -0500

    Agregando el archivo Entrada de datos

---
+++ 01.Elementos basicos/009.Entrada de datos.py
@@ -0,0 +1,6 @@
+nombre = input("¿Cual es tu nombre?: ")     #En caso de simples str solo usamos input
+print(f"Hola {nombre}")
+numero = int(input("Digite un número: "))       #En caso de un número debemos definir de que tipo es
+print(f"El número es: {numero+1}")
+numero = float(input("Digite un número: "))
+print(f"El número es: {numero}")
