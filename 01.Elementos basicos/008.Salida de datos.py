63afdcf6f8bd9f4c15e09ccc0c9e36225c39cc99
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:32:31 -0500

    Agregando el archivo Salida de datos

---
+++ 01.Elementos basicos/008.Salida de datos.py
@@ -0,0 +1,5 @@
+nombre = "Julián"
+edad = 18
+print("Hola",nombre,"tienes",edad,"años")       #Python inmediatamente añade los espacios necesarios
+print("Hola {} tienes {} años".format(nombre,edad))
+print(f"Hola {nombre} tienes {edad} años")
