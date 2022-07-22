9c28d3af9e751b9c133481f61c713f986f68e001
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:25:45 -0500

    Agregando el archivo Comentarios

---
+++ 01.Elementos basicos/003.Comentarios.py
@@ -0,0 +1,6 @@
+#Los comentarios de una sola linea solo se escriben con #
+#Los comentarios multilinea se escriben con tres comillas '''
+'''
+    Los comentarios multilinea son utiles para evitar ocupar mucho espacio
+    y hacer scroll lateral
+'''
