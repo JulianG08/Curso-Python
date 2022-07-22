6d9f8e58c92f27db184bbc89bca41fe889877e74
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:31:53 -0500

    Agregando el archivo Operadores de asignación

---
+++ 01.Elementos basicos/007.Operadores_de_asignacion.py
@@ -0,0 +1,17 @@
+#Sirven para ahorrar codigo
+a = 0
+#a = a + 5
+print(a)
+a += 5      #Suma en asignación
+print(a)
+a -= 2      #Resta en asignación
+print(a)
+a *= 3      #Multiplicación en asignación
+print(a)
+a /= 3      #División en asignación
+print(a)
+a **= 2     #Potencia en asignación
+print(a)
+a %= 2     #Modulo en asignación
+print(a)
+#Primero toca definir la variable o Python no sabra a que asignar los valores
