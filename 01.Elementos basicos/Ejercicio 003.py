ea2bf8bfd2c8815f353f2c82516b876eb2c2fcc6
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:36:41 -0500

    Agregando el archivo Ejercicio 003

---
+++ 01.Elementos basicos/Ejercicio 003.py
@@ -0,0 +1,7 @@
+'''
+    Se quiere intercambiar los valores de las variables
+'''
+a = input("Digita el valor de a: ")
+b = input("Digita el valor de b: ")
+a , b = b , a       #Se asignan en orden
+print(f"El nuevo valor de a es {a}\nEl nuevo valor de b es {b}")
