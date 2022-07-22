799381a4b6e4c9d788b33a649639a80aaf70d519
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:34:20 -0500

    Agregando el archivo Funciones integradas

---
+++ 01.Elementos basicos/010.Funciones integradas.py
@@ -0,0 +1,28 @@
+#Sirven para hacer conversiones entre tipo de datos
+n = int("10")
+print(n)
+#Para pasar de un valor numerico a un str debemos aplicar la función integrada str
+n = str(10)
+print(n)
+n = str(10.54)
+print(n)
+#De valor numerico a binario usamos bin
+n = bin(10)
+print(n)
+#Tambien aplica a hexadecimal con hex
+n = hex(10)
+print(n)
+n = int("0b1010",2)
+print(n)
+n = int("0xa",16)
+print(n)
+#Tambien existe una para el valor absoluto
+n = abs(-8)
+print(n)
+n = round(5.6)      #Sirve para redondear al más grande
+print(n)
+n = round(5.4)
+print(n)
+#Len sirve para contar los caracteres de una cadena
+n = len("Julián")
+print(n)
