c2e7dd87d60eb095485257d4953600ece728ffdb
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:28:44 -0500

    Agregando el archivo Operadores aritmeticos

---
+++ 01.Elementos basicos/004.Operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+#Permiten la realización de operaciones matematicas con las diferentes variables
+num01 = 10
+num02 = 5
+resultado01 = num01 + num02
+resultado02 = 10 + 5
+print(resultado01,resultado02)
+resultado03 = num01 * num02
+resultado04 = num01 / num02     #Entrega un número flotante
+print(resultado03,resultado04)
+num03 = 3
+resultado05 = num01 / num03
+resultado06 = num01 // num03        #Entrega un número entero redondeado a la baja
+resultado07 = num01 % num03     #Entrega el resto de la división
+print(resultado05,resultado06,resultado07)
+num04 = 2
+num05 = 5
+resultado08 = num04 ** num05
+print(resultado08)
+resultado09 = 3**3 * (13/5 - (2*4))     #Son totalmente combinables
+print(resultado09)
