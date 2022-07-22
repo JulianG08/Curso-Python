829f086e03307738543a12c750dbe5906903350f
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:29:56 -0500

    Agregando el archivo Operadores relacionales

---
+++ 01.Elementos basicos/005.Operadores_relacionales.py
@@ -0,0 +1,23 @@
+#Se utilizan para establecer una relación entre 2 valores, es para compararlos y produce un booleano
+#Todos tienen la misma prioridad por lo que se ejecutan de izquierda a derecha
+#Los aritmeticos tienen mayor prioridad
+'''
+    Estos son >, <, >=, <=, != y ==
+'''
+resultado01 = 10 < 20
+print(resultado01)
+resultado01 = 10 > 20
+print(resultado01)
+resultado01 = 10 == 20
+print(resultado01)
+resultado01 = 10 != 20
+print(resultado01)
+resultado01 = 10 <= 20
+print(resultado01)
+resultado01 = 10 >= 20
+print(resultado01)
+a = 10
+b = 20
+c = 30
+resultado01 = a + b == c
+print(resultado01)
