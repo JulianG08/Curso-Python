f1c57811f6c1eca8879b6384c47efb6231649ab3
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:25:05 -0500

    Agregando el archivo Asignación de valores

---
+++ 01.Elementos basicos/002.Asignacion_de_valores.py
@@ -0,0 +1,21 @@
+Numero01 = 10     #Para asignar un valor a una variable solo digitamos el nombre que le vamos a dar, añadimos el = y agregamos el valor
+print(Numero01)       #Para imprimir una variable basta con escribir su nombre dentro de la función print, pero sin las "" o imprimira el texto
+print(type(Numero01))     #La función type sirve para ver el tipo de variable que tenemos
+Numero02 = 12.74
+print(Numero02)
+print(type(Numero02))
+Cadena01 = "Julián"
+print(Cadena01)
+print(type(Cadena01))
+Valor01 = True      #Los valores booleanos deben empezarse con mayuscula
+print(Valor01)
+print(type(Valor01))
+Valor02 = False
+print(Valor02)
+print(type(Valor02))
+Suma = Numero01+Numero02        #Se pueden agregar varias operaciones en una sola variable, se ejacutan por orden de prioridad como lo hace la matematica normalmente
+print("El resultado de la suma es: ",Suma)      #Para agregar texto e imprimir inmediatamente una variable simplemente debemos agregar una ,
+valor = 10      #A esto se refiere el tipado dinamico, en un momento puedo almacenar un valor en una variable y luego puedo cambiarlo por otro totalmente diferente sin afectar lo ya escrito anteriormente
+print(valor)
+valor = "Julián"
+print(valor)
