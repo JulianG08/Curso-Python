4197b92851275b6a12e7e398faa1eebba9e7d166
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-22 18:24:08 -0500

    Agregando el archivo Hola mundo

---
+++ 01.Elementos basicos/001.Hola_mundo.py
@@ -0,0 +1,3 @@
+print("Hola mundo")         #Para imprimir o ver algo por pantalla usamos la función print
+print("Mi nombre es Julián")
+print("Hola mundo\nMi nombre es Julián")        #El \n para Python representa un salto de linea, de esta forma nos ahorramos otro print
