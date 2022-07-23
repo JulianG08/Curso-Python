5f1a06a79eab678752f1074d5f7efea5cd3169b9
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-23 14:19:58 -0500

    Agregando el archivo Bucle for tipo range

---
+++ 04.Bucles/003.Bucle for tipo range.py
@@ -0,0 +1,25 @@
+'''
+    Nos ayuda a determinar cuantas veces queremos que se
+    repita el bucle for
+
+    El rango simularia una colección
+'''
+
+for i in range(50):
+    print(i)
+
+print()
+
+for i in range(5,11):
+    print(i)
+
+print()
+
+#Numeros pares
+
+for i in range(2,21,2):
+    print(i)
+
+print()
+for i in range(20,1,-2):
+    print(i)
