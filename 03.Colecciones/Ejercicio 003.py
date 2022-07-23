5315cbc3b0039ae71e503e9c19850e8a48c00bcb
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2022-07-23 14:18:02 -0500

    Agregando el archivo Ejercicio 003

---
+++ 03.Colecciones/Ejercicio 003.py
@@ -0,0 +1,12 @@
+'''
+    Creando personajes dentro de una lista
+'''
+
+personajes = []     #Creando una lista vacía
+p = {"Nombre":"Aragorn","Clase":"Guerrero","Raza":"Dúnadan del Norte"}
+personajes.append(p)        #Agregando el personaje a la lista
+p = {"Nombre":"Gandalf","Clase":"Mago","Raza":"Istar"}
+personajes.append(p)
+p = {"Nombre":"Legolas","Clase":"Arquero","Raza":"Elfo Sindar"}
+personajes.append(p)
+print(personajes)
